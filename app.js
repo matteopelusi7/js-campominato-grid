d0dfd793b99cc635ad29c17d86dbd0e3c44763b3
matteopelusi7 <pelusimatteo02@gmail.com>
2022-02-16 15:16:11 +0100

    Modifiche generali

--- app.js
+++ app.js
@@ -1,68 +1,51 @@
 const gridWrapper = document.querySelector('.grid-container');
 const button = document.getElementById('button');
+const select = document.getElementById('livello');
 
 button.addEventListener('click', function() {
     
-    const select = document.getElementById('livello');
-    let typeSelect = select.value;
+    let rows,columns,sizeCell;
+
+    const typeSelect = select.value;
 
     switch (typeSelect) {
     
         case '0':
-            typeSelect = '0';
+            rows = columns= 10;
             break;
         case '1':
-            typeSelect = '1';
+            rows = columns= 9;
             break;
         case '2':
-            typeSelect = '2';
+            rows = columns= 7;
             break;
     
     }
 
-    if ( typeSelect == '0' ) {
+    function selected () {
 
-        for ( let i = 1; i <= 100; i++ ) {
-            
-            const grid = document.createElement('div');
-            grid.classList.add('grid');
-            grid.style.width = 'calc( 100% / 10 )'
-            grid.append(i);
-            
-            gridWrapper.appendChild(grid);
-            
-        }
+        this.classList.add('selected');
+
+        this.removeEventListener('click', selected);
 
     }
 
-    if ( typeSelect == '1' ) {
+    const totCell = rows * columns;
+    sizeCell = ` calc( 100% / ${columns} ) `;
+    
+    gridWrapper.innerHTML = '';
+    
+    for ( let i = 0; i < totCell; i++ ) {
+        
+        const grid = document.createElement('div');
+        grid.classList.add('grid');
+        grid.append(i + 1);
+        grid.style.width = sizeCell;
+            
+        gridWrapper.appendChild(grid);
 
-        for ( let i = 1; i <= 81; i++ ) {
+        grid.addEventListener ('click', selected);
             
-            const grid = document.createElement('div');
-            grid.classList.add('grid');
-            grid.style.width = 'calc( 100% / 9 )'
-            grid.append(i);
-            
-            gridWrapper.appendChild(grid);
-            
-        }
-
-    }
-
-    if ( typeSelect == '2' ) {
-
-        for ( let i = 1; i <= 49; i++ ) {
-            
-            const grid = document.createElement('div');
-            grid.classList.add('grid');
-            grid.style.width = 'calc( 100% / 7 )'
-            grid.append(i);
-            
-            gridWrapper.appendChild(grid);
-            
-        }
-
     }
 
 })
